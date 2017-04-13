41f87e305948e1d5e45951b850410d97da022bc9
Simon Besson-Girard <Simon Besson-Girard>
2017-04-13 22:27:15 +0200

    parsing bibfile for pmid of only articles

--- script.R
+++ script.R
@@ -2,5 +2,14 @@
 # Creation date: 20170413
 # Description: functions for analysing bibtex library (stats, graphs, ...)
 
-library(bibtex)
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 
+pathbib <- "library.bib"
+bib <- readChar(pathbib, file.info(pathbib)$size)
+bibart <- strsplit(bib, "[@]article") %>% unlist
+bibpmidand <- lapply(strsplit(bibart[-1], "pmid ="), function(x) return(x[2])) %>% unlist #-1 for export with header (mendeley)
+bibpmidbra <- lapply(strsplit(bibpmidand, ",\n"), function(x) return(x[1])) %>% unlist
+bibpmid <- bibpmidbra %>% sub("[{]", "", .) %>%  sub("[}]", "", .)
